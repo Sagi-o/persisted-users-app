81af91d3a4dbd2a0c9f042fb083beea88208c233
Sagi-o <sagioshri@gmail.com>
2026-05-15 13:04:36 +0300

    cors

--- apps/server/src/app/plugins/cors.ts
+++ apps/server/src/app/plugins/cors.ts
@@ -4,7 +4,10 @@ import cors from '@fastify/cors';
 
 export default fp(async function (fastify: FastifyInstance) {
   fastify.register(cors, {
-    origin: true,
+    origin: [
+      'http://localhost:4200',
+      'https://persisted-users-app-production.up.railway.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
 });
